60b613e81707c0fe2cf7456bd2026d6d939d784f
April King <april@mozilla.com>
2017-02-03 12:32:44 -0600

    Add site search, closes #9

--- httpobsdashboard/dist/js/httpobs-dashboard.js
+++ httpobsdashboard/dist/js/httpobs-dashboard.js
@@ -18,4 +18,26 @@ $(document).ready(function() {
             trigger: 'click'
         }
     )});
+
+    // Filter websites shown based on filter input
+    $('#filter-sites').on('keyup', function (e) {
+        var search = e.target.value;
+        // Clear previous search
+        $('table, tr').show();
+
+        $('tr td:first-of-type a:first-of-type').each(function (i, link) {
+            var linkName = link.textContent;
+            if (linkName.match(search)) {
+                return;
+            }
+            $(link).closest('tr').hide();
+            return;
+        });
+
+        $('tbody').each(function (i, tbody) {
+            if ($(tbody).children(':visible').length === 0) {
+                $(tbody).closest('table').hide();
+            }
+        });
+    });
 });
